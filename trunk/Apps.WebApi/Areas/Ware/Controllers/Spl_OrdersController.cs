d7e7297b743851dc838d38039a9c030df0b007e7
goodhlsh <35991667+goodhlsh@users.noreply.github.com>
2018-07-08 20:32:32 +0800

    f1.0.1

--- trunk/Apps.WebApi/Areas/Ware/Controllers/Spl_OrdersController.cs
+++ trunk/Apps.WebApi/Areas/Ware/Controllers/Spl_OrdersController.cs
@@ -1,7 +1,4 @@
-﻿using Aop.Api;
-using Aop.Api.Request;
-using Aop.Api.Response;
-using Apps.Common;
+﻿using Apps.Common;
 using Apps.IBLL;
 using Apps.Models;
 using Apps.Models.Spl;
@@ -121,28 +118,6 @@ namespace Apps.WebApi.Areas.Ware.Controllers
 
             return Json(spl_s);
 
-        }
-        public object alipay ()
-        {
-            //请求网关(gateway)，应用id(app_id)，应用私钥(private_key)，编码格式(charset)，支付宝公钥(alipay_public_key)，签名类型(sign_type)
-            //IAopClient client = new DefaultAopClient("https://openapi.alipay.com/gateway.do", APPID, APP_PRIVATE_KEY, "json", "1.0", "RSA2", ALIPAY_PUBLIC_KEY, CHARSET, false);
-            ////实例化具体API对应的request类,类名称和接口名称对应,当前调用接口名称如：alipay.open.public.template.message.industry.modify 
-            //AlipayOpenPublicTemplateMessageIndustryModifyRequest request = new AlipayOpenPublicTemplateMessageIndustryModifyRequest();
-            ////SDK已经封装掉了公共参数，这里只需要传入业务参数
-            ////此次只是参数展示，未进行字符串转义，实际情况下请转义
-            //request.BizContent = "{" +
-            //"    \"primary_industry_name\":\"IT科技/IT软件与服务\"," +
-            //"    \"primary_industry_code\":\"10001/20102\"," +
-            //"    \"secondary_industry_code\":\"10001/20102\"," +
-            //"    \"secondary_industry_name\":\"IT科技/IT软件与服务\"" +
-            //"  }";
-            //AlipayOpenPublicTemplateMessageIndustryModifyResponse response = client.execute(request);
-            ////调用成功，则处理业务逻辑
-            //if (response.isSuccess())
-            //{
-            //    //.....
-            //}
-            return null;
-        }
+        }       
     }
 }
